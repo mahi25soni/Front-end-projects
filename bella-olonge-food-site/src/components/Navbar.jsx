84ef320e1e9956727437cd21aff944dc62fc77da
Mahendra Soni <msdraj02@protonmail.com>
2023-07-04 05:09:52 +0530

    working on first

---
+++ bella-olonge-food-site/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../assets/logo.png";
+// import background from "../assets/background.png"
+import "../App.css";
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <img src={logo} alt="" />
+        <div className="navbarAnker">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Faq</a>
+          <a href="/">Payments</a>
+        </div>
+      </nav>
+      <div className="belowNav">
+        <div>
+          <h3 style={{ marginTop: "30px" }}>Food App</h3>
+          <h1>
+            Why stay hungary when you can <br />
+            order from Bella Olonje
+          </h1>
+          <h3>Download the Bella Olonje app now</h3>
+        </div>
+      </div>
+    </>
+  );
+}
